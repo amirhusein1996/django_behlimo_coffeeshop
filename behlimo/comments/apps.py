4dc4b21e9b8cda37a5d00a44d3f10f4572025bef
Amirhusein <amirhusein1996@gmail.com>
2023-08-04 12:42:16 +0330

    fixed /comments/apps.py

--- behlimo/comments/apps.py
+++ behlimo/comments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommentConfig(AppConfig):
-    name = 'comment'
+class CommentsConfig(AppConfig):
+    name = 'behlimo.comments'
     verbose_name = '5. ماژول کامنت'
