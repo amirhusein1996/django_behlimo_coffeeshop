45d8f2074133b7d3136e7b3d808347147b6e0eb7
Amirhusein <amirhusein1996@gmail.com>
2023-08-04 19:43:46 +0330

    Fix typo in 'map_adres' field, corrected to 'map_address'

--- behlimo/contact_us/models.py
+++ behlimo/contact_us/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
 class ContactUs(models.Model):
-    mobile = models.CharField(max_length=15,null=True,blank=True,verbose_name="شماره موبایل")
-    phone = models.CharField(max_length=15,null=True,blank=True,verbose_name="شماره تلفن")
-    email = models.CharField(max_length=50,null=True,blank=True,verbose_name="ایمیل")
-    address = models.CharField(max_length=200,null=True,blank=True,verbose_name="آدرس")
-    instagram = models.CharField(max_length=40,null=True,blank=True,verbose_name="اینستاگرام")
-    whatsapp = models.CharField(max_length=40,null=True,blank=True,verbose_name="واتساپ")
-    map_addres = models.CharField(max_length=350,null=True,blank=True,verbose_name="آدرس نقشه")
-    
-    
+    mobile = models.CharField(max_length=15, null=True, blank=True, verbose_name="شماره موبایل")
+    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name="شماره تلفن")
+    email = models.CharField(max_length=50, null=True, blank=True, verbose_name="ایمیل")
+    address = models.CharField(max_length=200, null=True, blank=True, verbose_name="آدرس")
+    instagram = models.CharField(max_length=40, null=True, blank=True, verbose_name="اینستاگرام")
+    whatsapp = models.CharField(max_length=40, null=True, blank=True, verbose_name="واتساپ")
+    map_address = models.CharField(max_length=350, null=True, blank=True, verbose_name="آدرس نقشه")
 
     class Meta:
         verbose_name = ("تنظیمات تماس  با ما")
@@ -18,4 +16,3 @@ class ContactUs(models.Model):
 
     def __str__(self):
         return "تنظیمات تماس"
-
