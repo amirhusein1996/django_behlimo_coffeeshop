8d0bc135f67bf393bffff6f22de6bf602ae919da
Amirhusein <amirhusein1996@gmail.com>
2023-08-05 00:11:48 +0330

    urlpatterns fixed

--- behlimo/menu/urls.py
+++ behlimo/menu/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.menu, name='menu'),
-    path('search/',views.search, name='search'),
-    path('<slug:category_slug>/', views.menu, name='menu_by_category'),
-    path('<slug:category_slug>/<slug:item_slug>/', views.item_detail, name='item_detail'),
-    path('messages', views.messages, name='messages'),
+    path('', views.MenuListView, name='menu'),
+    path('search/',views.SearchView.as_view(), name='search'),
+    path('<slug:category_slug>/', views.MenuListView.as_view(), name='menu_by_category'),
+    path('<slug:category_slug>/<slug:item_slug>/', views.ItemDetailView.as_view(), name='item_detail'),
+    path('messages', views.MessageView.as_view(), name='messages'),
     
 
 
