dfc20d54332ff989334dc439ff3fcfc1e18d4f7c
Amirhusein <amirhusein1996@gmail.com>
2023-08-04 13:51:59 +0330

    modified quantity field of CartItem

--- behlimo/cart/models.py
+++ behlimo/cart/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from menu.models import Menu
 
+
 # Create your models here.
 class Cart(models.Model):
-    cart_id = models.CharField(max_length=250, blank=True, verbose_name='شناسه سبد',null=True)
-    dete_added = models.DateField(auto_now_add=True,verbose_name='تاریخ ادد')
+    cart_id = models.CharField(max_length=250, blank=True, verbose_name='شناسه سبد', null=True)
+    dete_added = models.DateField(auto_now_add=True, verbose_name='تاریخ ادد')
 
     class Meta:
         verbose_name = "سبد"
@@ -13,12 +14,12 @@ class Cart(models.Model):
     def __str__(self):
         return self.cart_id
 
-    
+
 class CartItem(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, verbose_name='آیتم منو')
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name='سبد')
-    quantity = models.IntegerField(verbose_name='تعداد')
-    is_active = models.BooleanField(default=True,  verbose_name='فعال')
+    quantity = models.SmallIntegerField(verbose_name='تعداد', default=0)
+    is_active = models.BooleanField(default=True, verbose_name='فعال')
 
     class Meta:
         verbose_name = "آیتم سبد"
